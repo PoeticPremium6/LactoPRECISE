eeebb09576b1fd8ca8fce2e2120c3fe398b12111
Jonathan Josephs-Spaulding <49744158+PoeticPremium6@users.noreply.github.com>
2023-06-26 10:43:55 +0200

    Update 2_ICA_Dimensionality.py

--- Scripts/2_ICA_Dimensionality.py
+++ Scripts/2_ICA_Dimensionality.py
@@ -1,1 +1,66 @@
+from pymodulon.core import IcaData
+import os
+import pandas as pd
+from os import path
+import matplotlib.pyplot as plt
+from scipy import stats
+import numpy as np
+from tqdm.notebook import tqdm
 
+# Directory containing ICA outputs
+
+DATA_DIR = path.join('..','data','interim','ica_runs1')
+
+#Load Data
+def load_M(dim):
+    return pd.read_csv(os.path.join(DATA_DIR,str(dim),'S.csv'),index_col=0)
+
+def load_A(dim):
+    return pd.read_csv(os.path.join(DATA_DIR,str(dim),'A.csv'),index_col=0)
+
+dims = sorted([int(x) for x in os.listdir(DATA_DIR)])
+M_data = [load_M(dim) for dim in dims]
+A_data = [load_A(dim) for dim in dims]
+n_components = [m.shape[1] for m in M_data]
+
+#Compare Dimensionalities
+final_m = M_data[-1]
+thresh = 0.7
+n_final_mods = []
+for m in tqdm(M_data):
+    corrs = pd.DataFrame(index=final_m.columns,columns=m.columns)
+    for col1 in final_m.columns:
+        for col2 in m.columns:
+            corrs.loc[col1,col2] = abs(stats.pearsonr(final_m[col1],m[col2])[0])
+    n_final_mods.append(len(np.where(corrs > thresh)[0]))
+
+#Identify any Single-gene iModulons
+n_single_genes = []
+for m in tqdm(M_data):
+    counter = 0
+    for col in m.columns:
+        sorted_genes = abs(m[col]).sort_values(ascending=False)
+        if sorted_genes.iloc[0] > 2 * sorted_genes.iloc[1]:
+            counter += 1
+    n_single_genes.append(counter)
+
+#Plot Components
+non_single_components = np.array(n_components) - np.array(n_single_genes)
+DF_stats = pd.DataFrame([n_components,n_final_mods,non_single_components,n_single_genes],
+                        index=['Robust Components','Final Components','Multi-gene Components',
+                        'Single Gene Components'],
+                        columns=dims).T
+DF_stats.sort_index(inplace=True)
+dimensionality = DF_stats[DF_stats['Final Components'] >= DF_stats['Multi-gene Components']].iloc[0].name
+print('Optimal Dimensionality:',dimensionality)
+
+plt.plot(dims,n_components,label='Robust Components')
+plt.plot(dims,n_final_mods,label='Final Components')
+plt.plot(dims,non_single_components,label='Non-single-gene Components')
+plt.plot(dims,n_single_genes,label='Single Gene Components')
+
+plt.vlines(dimensionality,0,max(n_components),linestyle='dashed')
+
+plt.xlabel('Dimensionality')
+plt.ylabel('# Components')
+plt.legend(bbox_to_anchor=(1,1))
